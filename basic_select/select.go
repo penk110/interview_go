cb5584ab1b62b390797715676944977ef52eb8e7
penk110 <18177761907@163.com>
2021-11-04 01:21:10 +0800

    feat: select 一轮、二轮循环以及源码阅读

--- basic_select/select.go
+++ basic_select/select.go
@@ -17,6 +17,16 @@ import (
 	2.堵塞与控制，没有任何通道准备好，则进入阻塞
 		常用于：1)方法返回; 2)
 	3.default 存在该分支时如其它通道均阻塞则执行default分支
+
+
+	select一轮循环:
+		一轮循环，对所有scase循环(caseNil caseRecv caseSend caseDefault)，找到已就绪的通道(caseNil会被忽略)，caseDefault默认执行
+	select二轮循环:
+		一轮循环未退出，对当前协程需要进入休眠状态，并等待select至少有一个通道被唤醒，读取、写入 通道都需创建新的sudog并将其放到指定通道的等待队列中，之后当前协程进入休眠状态
+
+	源码：
+		runtime/select.go
+		func selectgo(cas0 *scase, order0 *uint16, pc0 *uintptr, nsends, nrecvs int, block bool) (int, bool) {}
 */
 
 func Worker(wid string, c chan string) {
@@ -50,19 +60,19 @@ func main() {
 
 	}
 	/*
-	default case
-	default case
-	default case
-	worker id: 123 rcv: 1635786067
-	default case
-	default case
-	worker id: 123 rcv: 1635786069
-	default case
-	default case
-	worker id: 123 rcv: 1635786071
-	default case
-	default case
-	worker id: 123 rcv: 1635786073
-	default case
+		default case
+		default case
+		default case
+		worker id: 123 rcv: 1635786067
+		default case
+		default case
+		worker id: 123 rcv: 1635786069
+		default case
+		default case
+		worker id: 123 rcv: 1635786071
+		default case
+		default case
+		worker id: 123 rcv: 1635786073
+		default case
 	*/
 }
